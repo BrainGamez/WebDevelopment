d32ae7810ea9628673581da8806def29764fc7bd
braingamez <yogibryan@hotmail.com>
2022-02-15 23:33:30 +0100

    first commit

---
+++ 02_Templates/09_Parse_Data/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+)
+
+func main() {
+	tpl, err := template.ParseGlob("templates/*.gohtml")
+	if err != nil {
+		log.Fatal(err)
+	}
+	err = tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", 42)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
