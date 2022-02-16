25ebb19467b8c62b2bb00e2cc4967d3d1f260c81
braingamez <yogibryan@hotmail.com>
2022-02-16 23:11:15 +0100

    15-02-2022

---
+++ 02_Templates/13_Pipelines_in_templates/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"log"
+	"os"
+	"text/template"
+	"time"
+)
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.New("").Funcs(fm).ParseGlob("templates/*.gohtml"))
+}
+
+func dayMonthYear(t time.Time) string {
+	return t.Format("02-01-2006")
+}
+
+var fm = template.FuncMap{
+	"fDayMontYear": dayMonthYear,
+}
+
+func main() {
+	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", time.Now())
+	if err != nil {
+		log.Fatal(err)
+	}
+}
