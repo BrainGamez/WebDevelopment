aafd2f1d024e3974e1e43bb74c7fbaeeff54947f
braingamez <yogibryan@hotmail.com>
2022-02-20 21:13:58 +0100

    20-02-2022: Exploring response writer

---
+++ 04_Understanding_net_http_package/34_Explore_*http.ResponseWriter/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type attachedType int
+
+func (m attachedType) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	w.Header().Set("Bryan-Key", "This is from Bryan")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	_, err := fmt.Fprintln(w, "<h1>Any code you want in this func</h1>")
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
+
+func main() {
+	var d attachedType
+	err := http.ListenAndServe(":8080", d)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
